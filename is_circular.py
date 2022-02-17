654cc51ab79f854116d225ac363df71004e7c0d2
cikay <muzaffer123cikay@gmail.com>
2022-02-17 21:14:59 +0300

    Bind variables to id of nodes

--- is_circular.py
+++ is_circular.py
@@ -3,8 +3,10 @@ def is_circular(node, next_attr, id_attr):
     if not next_node:
         return False
 
-    while getattr(next_node, next_attr) is not None and getattr(node, id_attr) != getattr(next_node, id_attr):
+    checked_node_id = getattr(node, id_attr)
+    next_node_id = getattr(next_node, id_attr)
+    while getattr(next_node, next_attr) is not None and checked_node_id != next_node_id:
         next_node = getattr(next_node, next_attr)
 
-    return getattr(node, id_attr) == getattr(next_node, id_attr)
+    return checked_node_id == next_node_id
 
