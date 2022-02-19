6c6fe205a05cf3768da64e85de6d070f6f7d0f75
cikay <muzaffer123cikay@gmail.com>
2022-02-19 18:45:44 +0300

    Add test a linked list to test

--- dynamic_attr_generator.py
+++ dynamic_attr_generator.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from typing import Any
+
 def dynamic_attr_generator(iterator, next_attr):
     if not iterator:
         return
@@ -7,3 +10,18 @@ def dynamic_attr_generator(iterator, next_attr):
 
     yield from dynamic_attr_generator(iterator, next_attr)
 
+
+@dataclass
+class Node:
+    id: Any
+    data: Any
+    parent: Any = None
+
+
+cell = Node(data="Cell", id=3)
+human = Node(data="Human", id=2, parent=cell)
+homo_sapiens = Node(data="Homo sapiens", id=1, parent=human)
+
+iterator = dynamic_attr_generator(homo_sapiens, 'parent')
+for node in iterator:
+    print(f'{node.data}-{node.id}')
